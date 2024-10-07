83d37857fdf20d1d7a6d145b8abb72a1952a3f01
hshehabu <hamzashehabu457@gmail.com>
2024-10-07 15:08:51 +0300

    assets relocation

--- appointment-booking/src/App.jsx
+++ appointment-booking/src/App.jsx
@@ -9,10 +9,12 @@ import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
     <div className="mx-4 sm:mx-[10%]">
+      <Navbar/>
       <Routes>
         <Route path ="/" element={<Home/>}/>
         <Route path ="/doctors" element={<Doctors/>}/>
---
+++ appointment-booking/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {assets} from '../assets/assets_frontend'
+const Navbar = () => {
+  return (
+    <div>
+        <img src={assets.logo} alt="" />
+    </div>
+  )
+}
+
+export default Navbar
