b1d1dedd27980df5010a1c3bb2e9bad566b2edfd
hshehabu <hamzashehabu457@gmail.com>
2024-10-07 15:00:45 +0300

    round 2 initial setup and route definition

--- appointment-booking/src/App.jsx
+++ appointment-booking/src/App.jsx
@@ -1,34 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Doctors from './pages/Doctors'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="mx-4 sm:mx-[10%]">
+      <Routes>
+        <Route path ="/" element={<Home/>}/>
+        <Route path ="/doctors" element={<Doctors/>}/>
+        <Route path ="/doctors/:speciaility" element={<Doctors/>}/>
+        <Route path ="/login" element={<Login/>}/>
+        <Route path ="/about" element={<About/>}/>
+        <Route path ="/contact" element={<Contact/>}/>
+        <Route path ="/my-profile" element={<MyProfile/>}/>
+        <Route path ="/my-appointments" element={<MyAppointments/>}/>
+        <Route path ="/my-appointments/:docId" element={<Appointment/>}/>
+      </Routes>
+    </div>
   )
 }
 
---
+++ appointment-booking/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-primary'>Home page</div>
+  )
+}
+
+export default Home
