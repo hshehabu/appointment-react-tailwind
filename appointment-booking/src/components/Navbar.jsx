e8250d3a0a3839aeff58dba47df6db082af7b5cd
hshehabu <hamzashehabu457@gmail.com>
2024-10-09 09:37:32 +0300

    dropdown menu feature

--- appointment-booking/src/components/Navbar.jsx
+++ appointment-booking/src/components/Navbar.jsx
@@ -34,7 +34,7 @@ const Navbar = () => {
           <div className="flex items-center gap-2  cursor-pointer group relative">
             <img src={assets.profile_pic} className="rounded-full w-8" alt="" />
             <img src={assets.dropdown_icon} className="w-2.5" alt="" />
-            <div>
+            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-10 hidden group-hover:block ">
                 <div>
                     <p>My Profile</p>
                     <p>My Appointments</p>
